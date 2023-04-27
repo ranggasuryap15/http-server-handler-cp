75219d06887408b0b37dd5ebebf46a19c5bf4a8c
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-04-27 16:08:26 +0700

    slimpify hari

--- main.go
+++ main.go
@@ -9,29 +9,13 @@ import (
 func GetHandler() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		now := time.Now()
-		hari := ""
-		switch now.Weekday() {
-		case time.Sunday:
-			hari = "Sunday"
-		case time.Monday:
-			hari = "Monday"
-		case time.Tuesday:
-			hari = "Tuesday"
-		case time.Wednesday:
-			hari = "Wednesday"
-		case time.Thursday:
-			hari = "Thursday"
-		case time.Friday:
-			hari = "Friday"
-		case time.Saturday:
-			hari = "Saturday"
-		}
 
 		day := now.Day()
 		month := now.Month()
 		year := now.Year()
 
-		result := fmt.Sprintf("%s, %d %s %d", hari, day, month, year)
+		fmt.Println(time.Monday)
+		result := fmt.Sprintf("%s, %d %s %d", now.Weekday(), day, month, year)
 		writer.Write([]byte(result))
 	} // TODO: replace this
 }
